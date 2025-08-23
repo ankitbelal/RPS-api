9a2b1f4d3ca46f9b62553b9fb61c6ba54121a3da
Ankit Belal <aankitbelal@gmail.com>
2025-08-23 23:11:44 +0545

    remove api versioning

--- routes/auth/auth.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\auth\AuthController;
-
-Route::post('/login', [AuthController::class, 'login']);

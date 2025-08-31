dc4cba05628650e2c0a04ef67ef864d8439924f7
Ankit Belal <aankitbelal@gmail.com>
2025-08-31 16:51:42 +0545

    fixed bulk register api

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,9 @@ Route::post('/logout', [AuthController::class, 'logout']);
         Route::get('/download-sample', [ProgramController::class, 'downloadSampleProgram']);
         Route::post('/register', [ProgramController::class, 'store']);
         Route::get('/list', [ProgramController::class, 'index']);
-        Route::get('/{id}', [ProgramController::class, 'show']);
-        Route::put('/{id}', [ProgramController::class, 'update']);
-        Route::delete('/{id}', [ProgramController::class, 'destroy']);
+        // Route::get('/{id}', [ProgramController::class, 'show']);
+        // Route::put('/{id}', [ProgramController::class, 'update']);
+        // Route::delete('/{id}', [ProgramController::class, 'destroy']);
        
 
 

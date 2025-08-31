51623b4481b9303cf95a9552c8120774a02892b4
Ankit Belal <aankitbelal@gmail.com>
2025-08-31 13:20:03 +0545

    added image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -366,8 +366,8 @@
                     </div>
                     
                     <div class="developer">
-                        <img src="https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=100&q=80" alt="Arya Singh" class="developer-img">
-                        <div class="developer-name">Arya Singh</div>
+                        <img src="{{ asset('devs/aarya.jpg') }}" alt="Arya Singh" class="developer-img">
+                        <div class="developer-name">Aarya Singh</div>
                         <div class="developer-role">Frontend Developer</div>
                         <div class="developer-links">
                             <a href="https://github.com/aaryasingh12" title="GitHub"><i class="fab fa-github"></i></a>
